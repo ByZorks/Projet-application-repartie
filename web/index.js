733f4c56d22df215a25b7ed63122ac4991ac0141
David <80073731+ByZorks@users.noreply.github.com>
2025-06-10 14:59:32 +0200

    affichage simple map sur nancy

---
+++ web/index.js
@@ -0,0 +1,8 @@
+import L from 'leaflet';
+
+const map = L.map('map').setView([48.691959, 6.184008], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
