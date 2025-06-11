cccd497569ba61e18025ecbd75bd8bafd0fa1dd5
Math54500 <Maths54500z@gmail.com>
2025-06-11 16:17:42 +0200

    modif ServiceBD

--- rmiresto/src/ServiceData.java
+++ rmiresto/src/ServiceData.java
@@ -9,6 +9,7 @@ public interface ServiceData extends Remote {
     // Méthode pour récupérer un restaurant par son ID
     boolean addReservation(Reservation res) throws RemoteException;
 
-
+    // Méthode pour récupérer la première reservation par sa date et son heure
+    String getTables(String date, String heure) throws RemoteException, SQLException;
 
 }
