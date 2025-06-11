3053d31af44e07a0710bee1041ad2afa4773feee
David <80073731+ByZorks@users.noreply.github.com>
2025-06-11 14:07:14 +0200

    afficher restaurants sur la map

--- web/index.js
+++ web/index.js
@@ -1,11 +1,19 @@
 import L from 'leaflet';
-require('dotenv').config();
+import urls from './js/env.js';
 
 const map = initMap();
+
+// Handler pour afficher velibs
 const boutonLoadVelib = document.getElementById("loadVelib");
-boutonLoadVelib.addEventListener("click",(e)=>{
+boutonLoadVelib.addEventListener("click", (e) => {
     displayVelibs(map)
 });
+
+// Handler pour afficher les restaurants
+const boutonLoadRestos = document.getElementById("loadRestos");
+boutonLoadRestos.addEventListener("click", () => {
+    displayRestos(map);
+})
 
 /**
  * Initialise la carte Leaflet.
@@ -14,12 +22,50 @@ boutonLoadVelib.addEventListener("click",(e)=>{
 function initMap() {
     const map = L.map('map').setView([48.691959, 6.184008], 13);
 
-    const tiles = L.tileLayer(process.env.TILE_LAYER_URL, {
+    const tiles = L.tileLayer(urls.TILE_LAYER_URL, {
         maxZoom: 19,
-        attribution: "&copy; <a href='${process.env.TILE_LAYER_ATTRIBUTION}'>OpenStreetMap</a>"
+        attribution: "&copy; <a href='${urls.TILE_LAYER_ATTRIBUTION}'>OpenStreetMap</a>"
     }).addTo(map);
 
     return map;
+}
+
+/**
+ * Affiche les marqueurs des restaurants sur la carte.
+ * @param map - La carte Leaflet sur laquelle afficher les marqueurs.
+ * @returns {Promise<void>}
+ */
+async function displayRestos(map) {
+    const restos = await fetchRestos();
+    if (restos) {
+        restos.forEach((resto) => {
+            const name = resto.nom;
+            const address = resto.adresse;
+            const lat = resto.latitude;
+            const lon = resto.longitude;
+            const marker = L.marker([lat, lon]).addTo(map);
+            marker.bindPopup(`
+                <strong>${name}</strong><br>
+                Adresse: ${address}<br>
+            `);
+        })
+    }
+}
+
+/**
+ * Récupère les données des restaurants depuis l'API.
+ * @returns {Promise<any>} Retourne les données des restaurants
+ */
+async function fetchRestos() {
+    try {
+        const response = await fetch(`${urls.API_RESTOS_URL}`);
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (error) {
+        console.error("Erreur fetch velibs: ", error);
+    }
+
 }
 
 /**
@@ -30,8 +76,8 @@ function initMap() {
 async function displayVelibs(map) {
     const velibs = await fetchVelibs();
     const infostation = await fetchStation();
-    if(velibs && infostation) {
-        for(let i = 0;i<velibs.data.stations.length;i++){
+    if (velibs && infostation) {
+        for (let i = 0; i < velibs.data.stations.length; i++) {
             const name = velibs.data.stations[i].name;
             const lat = velibs.data.stations[i].lat;
             const lon = velibs.data.stations[i].lon;
@@ -56,7 +102,7 @@ async function displayVelibs(map) {
  */
 async function fetchVelibs() {
     try {
-        const response = await fetch(`${procress.env.API_VELIBS_URL}`);
+        const response = await fetch(`${urls.API_VELIBS_URL}`);
         if (response.ok) {
             return await response.json();
         }
@@ -71,13 +117,13 @@ async function fetchVelibs() {
  * @returns {Promise<any>} Retourne les données des stations
  */
 
-async function fetchStation(){
-    try{
-        const response = await fetch(`${process.env.API_STATIONS_URL}`);
-        if(response.ok){
+async function fetchStation() {
+    try {
+        const response = await fetch(`${urls.API_STATIONS_URL}`);
+        if (response.ok) {
             return await response.json();
         }
-    }catch (error){
-        console.log("Erreur fetch stations : ",error);
+    } catch (error) {
+        console.log("Erreur fetch stations : ", error);
     }
 }
