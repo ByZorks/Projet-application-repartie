b4328fe3728b4a9fc7639e7fd4fb9009db5417c8
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2025-06-13 09:23:55 +0200

    correction de bug d'ajout de table

--- proxy/src/ServiceBD.java
+++ proxy/src/ServiceBD.java
@@ -3,8 +3,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServiceBD implements ServiceData{
+public class ServiceBD implements ServiceData {
     private Statement statement;
+
     // Constructeur
     public ServiceBD() throws ClassNotFoundException, SQLException {
         // Initialisation ou configuration si nécessaire
@@ -57,15 +58,49 @@ public class ServiceBD implements ServiceData{
     @Override
     public boolean addReservation(Reservation res) throws RemoteException {
         try {
-            String query = "INSERT INTO RESERVATIONS (RESTAURANT_ID, NOMBRE_CONVIVES, TELEPHONE, DATERES, NOM, PRENOM) VALUES ( ?, ?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = statement.getConnection().prepareStatement(query);
+            ArrayList<Integer> tables = getTables(res.getRestaurantId(), res.getDateReservation(), res.getNombreConvives());
+
+            if (tables.isEmpty()) throw new SQLException();
+
+            String query = "INSERT INTO RESERVATIONS (RESTAURANT_ID, NOMBRE_CONVIVES, TELEPHONE, DATERES, NOM, PRENOM) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = statement.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
             preparedStatement.setInt(1, res.getRestaurantId());
             preparedStatement.setInt(2, res.getNombreConvives());
             preparedStatement.setString(3, res.getTelephone());
             preparedStatement.setTimestamp(4, Timestamp.valueOf(res.getDateReservation()));
             preparedStatement.setString(5, res.getNom());
             preparedStatement.setString(6, res.getPrenom());
+
             preparedStatement.executeUpdate();
+
+            query = "SELECT * FROM RESERVATIONS WHERE RESTAURANT_ID = ? AND NOMBRE_CONVIVES = ? AND TELEPHONE = ? AND DATERES = ? AND NOM = ? AND PRENOM = ?";
+            preparedStatement = statement.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1, res.getRestaurantId());
+            preparedStatement.setInt(2, res.getNombreConvives());
+            preparedStatement.setString(3, res.getTelephone());
+            preparedStatement.setTimestamp(4, Timestamp.valueOf(res.getDateReservation()));
+            preparedStatement.setString(5, res.getNom());
+            preparedStatement.setString(6, res.getPrenom());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                for (int i : tables) {
+                    query = "INSERT INTO RESERVATIONTABLES (RESERVATION_ID, TABLE_ID) VALUES (?, ?)";
+                    preparedStatement = statement.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+                    preparedStatement.setInt(1, id);
+                    preparedStatement.setInt(2, i);
+                    preparedStatement.executeUpdate();
+                }
+            }
+
+
+
+
+
             return true;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -73,31 +108,44 @@ public class ServiceBD implements ServiceData{
         }
     }
 
-    public int getTables(String idresto, String date, String heure) throws RemoteException {
-        String query = "SELECT ID FROM TABLES " +
+    public ArrayList<Integer> getTables(int idresto, String date, int nb) throws RemoteException {
+        System.out.println("date : " + date);
+        String query = "SELECT ID,CAPACITE " +
+                "FROM TABLES " +
                 "WHERE RESTAURANT_ID = ? " +
-                "AND ID NOT IN (" +
-                "    SELECT TABLE_ID FROM reservations " +
+                "AND ID NOT IN ( " +
+                "    SELECT TABLE_ID " +
+                "    FROM reservations " +
                 "    NATURAL JOIN reservationtables " +
-                "    WHERE dateres = TO_DATE(?, 'DD-MM-YYYY HH24:MI')" +
+                "    WHERE dateres BETWEEN TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS') - INTERVAL '1' HOUR " +
+                "                     AND TO_DATE(?, 'YYYY-MM-DD HH24:MI:SS') + INTERVAL '1' HOUR " +
                 ")";
+
 
         try (
                 PreparedStatement preparedStatement = statement.getConnection().prepareStatement(query)
         ) {
-            preparedStatement.setInt(1, Integer.parseInt(idresto));
-            preparedStatement.setString(2, date + " " + heure);
+            preparedStatement.setInt(1, idresto);
+            preparedStatement.setString(2, date);
+            preparedStatement.setString(3, date);
 
             ResultSet resultSet = preparedStatement.executeQuery();
-            int id =-1;
-            if(resultSet.next()) {
-                id = resultSet.getInt("ID");
+            ArrayList<Integer> tables = new ArrayList<Integer>();
+            int sum = 0;
+            while (resultSet.next()) {
+                tables.add(resultSet.getInt("ID"));
+                sum += resultSet.getInt("CAPACITE");
+            }
+            System.out.println(sum);
+            if (sum < nb) {
+                throw new SQLException();
             }
             resultSet.close();
-            return id;
+            return tables;
 
         } catch (SQLException e) {
-            return -1;
+            e.printStackTrace();
+            return new ArrayList<>();
         }
     }
 
