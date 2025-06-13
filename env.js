b88bca415179552cc1a91337afa02d3fb0a2e8a4
Benjamin <benjamin.scheffer7@etu.univ-lorraine.fr>
2025-06-13 12:58:12 +0200

    lien entre les deux .html

---
+++ env.js
@@ -0,0 +1,10 @@
+const urls = {
+    TILE_LAYER_URL:"https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    TILE_LAYER_ATTRIBUTION:"http://www.openstreetmap.org/copyright",
+    API_VELIBS_URL:"https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_information.json",
+    API_STATIONS_URL:"https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_status.json",
+    API_RESTOS_URL:"http://100.64.80.205:8000/BD",
+    API_EVENTS_URL:"http://100.64.80.205:8000/evenement",
+};
+
+export default urls;
